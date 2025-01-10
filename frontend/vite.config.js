5ea8e64659dd005e88198a4723978f7efc8ed32f
santhoshT15 <ramsanthoshbms@gmail.com>
2025-01-10 12:05:38 +0530

    deployment-comiit-3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom','react-toastify'],
+      external: ['react-router-dom','react-toastify','axios'],
     },
   },
 })
