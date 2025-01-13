68f08d20f29cf68fcfa0953b0d54bbbb1e6b02fe
santhoshT15 <ramsanthoshbms@gmail.com>
2025-01-13 09:22:33 +0530

    comit 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-router-dom', 'react-toastify', 'axios'],
-  },
+  
 })
